be4769f40e378e1ee5c4f7ce4241039dccb340da
Gwoop <gwooparap@gmail.com>
2023-01-04 02:27:38 +0300

    Добавление базового конфиграционного файла - config.txt

--- Structs/Models.go
+++ Structs/Models.go
@@ -1,22 +1,31 @@
 package Structs
 
+//СТруктура для создания тестового пользователя
 type User struct {
-	Name     string //`json:"name"`
-	Lastname string //`json:"lastname"`
-	Sex      int    //`json:"sex"`
-	Birdh    string //`json:"birdh"`
-	Tel      string //`json:"tel"`
-	Chatid   int    //`json:"chatid"`
-	Email    string //`json:"email"`
-	Password string //`json:"password"`
+	Name     string //Имя
+	Lastname string //Фамилия
+	Sex      int    //Пол
+	Birdh    string //Дата рождения
+	Tel      string //номер телефона (логин)
+	Chatid   int    //Внешний ключ от таблицы чата
+	Email    string //Электронная почта
+	Password string //Пароль
 }
 
 type Token struct {
 	Token string `json:"token"`
 }
 
+//Структура ответа
 type ResponsesUser struct {
 	Id       int64  `json:"id"`
 	Login    string `json:"login"`
 	Password string `json:"password"`
+}
+
+//Структура для авторизации админа
+type Admin struct {
+	Id       int
+	Login    string
+	Password string
 }
